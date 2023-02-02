9150d74e8dc21c2c7ada3814b295edada488c132
nahuel <nahuelegaray@gmail.com>
2023-02-01 22:08:01 -0300

    primer tp

---
+++ ejercicioSuma.js
@@ -0,0 +1,10 @@
+let num1= 22;
+let num2= 31;
+
+function sumarNumeros(){
+    let resultado = num1 + num2;
+    return resultado;
+}
+
+let resultadoSuma = sumarNumeros();
+console.log(resultadoSuma);
